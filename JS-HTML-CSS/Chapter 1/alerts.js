28329bda8e16fbbc8dfd2a399edac4e698c3d259
Ron Kibel <ron.kibel1@gmail.com>
2022-09-13 20:20:17 -0700

    first lesson- basic datatypes

---
+++ JS-HTML-CSS/Chapter 1/alerts.js
@@ -0,0 +1,23 @@
+// let arr = [1, 2, 'etc', {hello: 'hi'}, true];
+// while(arr.length > 0){
+//     console.log(arr);
+//     arr.pop();
+// }
+
+// let temp;
+// console.log(temp, temp+3, `the temp is ${temp}`);
+
+// let temp = null;
+// console.log(temp, temp+3, `the temp is ${temp}`);
+
+// let arr = [];
+// for (let i = 0; i < 5; i++) {
+//     arr.push(i);
+// }
+// console.log(arr);
+
+let score = 'hello';
+let bool = Boolean('');
+console.log(bool);
+console.log(typeof score);
+console.log(typeof score === 'string');
