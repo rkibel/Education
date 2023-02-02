a065fdc25ba5fb4702dac41168cbece4fba612bf
Ron Kibel <ron.kibel1@gmail.com>
2023-02-02 12:24:49 -0800

    Update suspensionbridges.py

--- CMPSC130A/PA1/suspensionbridges.py
+++ CMPSC130A/PA1/suspensionbridges.py
@@ -1,3 +1,33 @@
+"""
+import numpy as np
+import math
+from sympy import *
+
+d, s = input().split()
+d = float(d)
+s = float(s)
+print(d, s)
+
+def l(a1, a2):
+    return 2 * float(a1) * (sinh(a2 / 2 / a1))
+
+x = symbols('x')
+f = x * cosh(d / (2*x)) - s - x
+
+def newtonsMethod(f, p0, tol, N):
+    global x
+    df = diff(f, x)
+    for i in range(N):
+        p = p0 - f.subs(x, p0) / df.subs(x, p0)
+        print("p" + str(i+1) + ": " + str(float(p)))
+        if abs(p - p0) < tol: 
+            return p
+        p0 = p
+
+a = newtonsMethod(f, d/2, 1e-8, 500)
+print("\n\n", a)
+print(l(a, d))
+"""
 import numpy as np
 import math
 
@@ -20,16 +50,16 @@ d, s = input().split()
 d = int(d)
 s = int(s)
 
-left = 7000
-right = 7000
+left = 70000
+right = 70000
 if (f(left) < 0):
     while(f(left) < 0):
-        left -= 100
+        left -= 1000
         if (left == 0): left = 0.04
-    right = left + 100
+    right = left + 1000
 else:
     while(f(right) > 0):
-        right += 100
-    left = right - 100
-a = bisection(f, left, right, 1e-8)
+        right += 1000
+    left = right - 1000
+a = bisection(f, 0.04, 100001, 1e-8)
 print(l(a, d))
