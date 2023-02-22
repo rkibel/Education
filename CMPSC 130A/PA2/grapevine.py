a65bcade14bfe719d15fc1cf6e7678181dcda921
Ron Kibel <ron.kibel1@gmail.com>
2023-02-21 17:49:25 -0800

    Update grapevine.py

--- CMPSC 130A/PA2/grapevine.py
+++ CMPSC 130A/PA2/grapevine.py
@@ -1,40 +1,44 @@
 import numpy as np
 
-people, connections, days = (int(i) for i in input().strip().split())
-peopleToIndices = dict()
-peopleToSkept = list()
-edges = [list() for i in range(people)]
-for i in range(people):
-    person, skepticism = input().strip().split()
-    peopleToIndices[person] = i
-    peopleToSkept.append(int(skepticism))
-for i in range(connections):
-    person1, person2 = input().strip().split()
-    edges[peopleToIndices[person1]].append(peopleToIndices[person2])
-    edges[peopleToIndices[person2]].append(peopleToIndices[person1])
-origin = peopleToIndices[input()]
+def main():
+    people, connections, days = (int(i) for i in input().strip().split())
+    peopleToIndices = dict()
+    peopleToSkept = list()
+    edges = [list() for i in range(people)]
+    for i in range(people):
+        person, skepticism = input().strip().split()
+        peopleToIndices[person] = i
+        peopleToSkept.append(int(skepticism))
+    for i in range(connections):
+        person1, person2 = input().strip().split()
+        edges[peopleToIndices[person1]].append(peopleToIndices[person2])
+        edges[peopleToIndices[person2]].append(peopleToIndices[person1])
+    origin = peopleToIndices[input()]
 
-# which people broadcast the rumor
-rumorers = [False for i in range(people)]
-rumorers[origin] = True
+    # which people broadcast the rumor
+    rumorers = [False for i in range(people)]
+    rumorers[origin] = True
 
-# which people recently heard the rumor
-newbies = set()
-newbies.add(origin)
+    # which people recently heard the rumor
+    newbies = set()
+    newbies.add(origin)
 
-# current running amount of skepticism
-currSkept = [0 for i in range(people)]
-currSkept[origin] = 1
+    # current running amount of skepticism
+    currSkept = [0 for i in range(people)]
+    currSkept[origin] = 1
 
-for i in range(days):
-    newerbies = set()
-    for _, newbie in enumerate(newbies):
-        rumorers[newbie] = True
-        for _, k in enumerate(edges[newbie]):
-            if rumorers[k]: continue
-            currSkept[k] += 1
-            if currSkept[k] >= peopleToSkept[k]:
-                newerbies.add(k)
-    newbies = newerbies
+    for i in range(days):
+        newerbies = set()
+        for _, newbie in enumerate(newbies):
+            rumorers[newbie] = True
+            for _, k in enumerate(edges[newbie]):
+                if rumorers[k]: continue
+                currSkept[k] += 1
+                if currSkept[k] >= peopleToSkept[k]:
+                    newerbies.add(k)
+        newbies = newerbies
 
-print(np.count_nonzero(currSkept) - 1)
+    print(np.count_nonzero(currSkept) - 1)
+    
+if __name__ == "__main__":
+    main()
